54fcc5c227ebe812b1783a435a094c8581633946
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-09-11 22:33:56 +0300

    Task8-Graph

--- src/s08/Task8.java
+++ src/s08/Task8.java
@@ -3,6 +3,43 @@ package s08;
 public class Task8 {
 
     public static void main(String[] args) {
+        SimpleGraph graph = new SimpleGraph(5);
+        System.out.println(graph);
+        for(int i = 1; i <= graph.max_vertex; i++){
+            graph.AddVertex(i);
+        }
+        System.out.println(graph);
+        graph.AddVertex(99);
+        System.out.println(graph);
+        System.out.println(graph.IsEdge(2, 3));
+
+        graph.AddEdge(1,2);
+        System.out.println(graph);
+        System.out.println("1,2: " + graph.IsEdge(1, 2));
+        graph.AddEdge(2,3);
+        System.out.println(graph);
+        System.out.println("2,3: " + graph.IsEdge(2, 3));
+        graph.AddEdge(3,3);
+        System.out.println(graph);
+        System.out.println("3,3: " + graph.IsEdge(3, 3));
+        graph.AddEdge(4,3);
+        System.out.println(graph);
+        System.out.println("4,3: " + graph.IsEdge(4, 3));
+        graph.RemoveVertex(3);
+        System.out.println(graph);
+        graph.AddVertex(66);
+        graph.AddEdge(4, 3);
+        System.out.println(graph);
+        graph.RemoveEdge(4,3);
+        System.out.println(graph);
+        graph.AddEdge(2,2);
+        System.out.println(graph.IsEdge(2, 3));
+        System.out.println(graph.IsEdge(2, 66));
+        System.out.println(graph.IsEdge(2, 2));
+        System.out.println(graph);
+
+
+
 
     }
 }
--- src/s08/Vertex.java
+++ src/s08/Vertex.java
@@ -1,54 +1,100 @@
 package s08;
 
-class Vertex
-{
+class Vertex {
     public int Value;
-    public Vertex(int val)
-    {
+
+    public Vertex(int val) {
         Value = val;
     }
 }
 
-class SimpleGraph
-{
-    Vertex [] vertex;
-    int [][] m_adjacency;
+class SimpleGraph {
+    Vertex[] vertex;
+    int[][] m_adjacency;
     int max_vertex;
 
-    public SimpleGraph(int size)
-    {
+    public SimpleGraph(int size) {
         max_vertex = size;
-        m_adjacency = new int [size][size];
+        m_adjacency = new int[size][size];
         vertex = new Vertex[size];
     }
 
-    public void AddVertex(int value)
-    {
-        // ваш код добавления новой вершины
-        // со значением value
-        // в незанятую позицию vertex
+    public void AddVertex(int value) {
+        Vertex node = new Vertex(value);
+        for (int i = 0; i < vertex.length; i++) {
+            if (vertex[i] == null) {
+                vertex[i] = node;
+                return;
+            }
+        }
     }
 
-    // здесь и далее, параметры v -- индекс вершины
-    // в списке  vertex
-    public void RemoveVertex(int v)
-    {
-        // ваш код удаления вершины со всеми её рёбрами
+
+    public void RemoveVertex(int v) {
+
+        if (v >= vertex.length) {
+            return;
+        }
+        vertex[v] = null;
+        for (int i = 0; i < m_adjacency.length; i++) {
+            m_adjacency[i][v] = 0;
+        }
+        for (int i = 0; i < m_adjacency[0].length; i++) {
+            m_adjacency[v][i] = 0;
+        }
     }
 
-    public boolean IsEdge(int v1, int v2)
-    {
-        // true если есть ребро между вершинами v1 и v2
-        return false;
+    public boolean IsEdge(int v1, int v2) {
+
+        if (v1 >= vertex.length || v2 >= vertex.length) {
+            return false;
+        }
+        return m_adjacency[v1][v2] == 1 && m_adjacency[v2][v1] == 1;
     }
 
-    public void AddEdge(int v1, int v2)
-    {
-        // добавление ребра между вершинами v1 и v2
+    public void AddEdge(int v1, int v2) {
+
+        if (IsEdge(v1, v2)) {
+            return;
+        }
+        if (v1 < vertex.length && v2 < vertex.length) {
+            m_adjacency[v1][v2] = 1;
+            m_adjacency[v2][v1] = 1;
+        }
     }
 
-    public void RemoveEdge(int v1, int v2)
-    {
-        // удаление ребра между вершинами v1 и v2
+
+    public void RemoveEdge(int v1, int v2) {
+
+        if (!IsEdge(v1, v2)) {
+            return;
+        }
+        if (v1 < vertex.length && v2 < vertex.length) {
+            m_adjacency[v1][v2] = 0;
+            m_adjacency[v2][v1] = 0;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        System.out.println("SimpleGraph{");
+        for (int i = 0; i < m_adjacency.length; i++) {
+            System.out.print((i) + ":");
+            for (int j = 0; j < m_adjacency[0].length; j++) {
+                System.out.print(m_adjacency[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("vertex = ");
+        for (Vertex node : vertex) {
+            if (node != null) {
+                System.out.print(node.Value + ", ");
+            } else {
+                System.out.print(null + ", ");
+            }
+        }
+        return "max_vertex = " + max_vertex +
+                '}';
     }
 }
