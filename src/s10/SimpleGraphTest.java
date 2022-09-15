cbae65ac430426828e0f796580ff9616fdc53f7b
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-09-15 17:24:12 +0300

    Task10 test

--- src/s10/SimpleGraphTest.java
+++ src/s10/SimpleGraphTest.java
@@ -11,17 +11,21 @@ class SimpleGraphTest {
     void depthFirstSearch() {
         ArrayList<Vertex> list = new ArrayList<>();
         SimpleGraph graph = new SimpleGraph(5);
-        graph.AddEdge(0, 1);
-        graph.AddEdge(0, 2);
-        graph.AddEdge(0, 3);
-
-        graph.AddEdge(1, 3);
-        graph.AddEdge(1, 4);
-
-        graph.AddEdge(2, 3);
-
-        graph.AddEdge(3, 3);
-        graph.AddEdge(3, 4);
+        graph.AddEdge(0,1);
+        graph.AddEdge(0,2);
+        graph.AddEdge(0,3);
+        graph.AddEdge(1,0);
+        graph.AddEdge(1,3);
+        graph.AddEdge(1,4);
+        graph.AddEdge(2,0);
+        graph.AddEdge(2,3);
+        graph.AddEdge(3,0);
+        graph.AddEdge(3,1);
+        graph.AddEdge(3,2);
+        graph.AddEdge(3,3);
+        graph.AddEdge(3,4);
+        graph.AddEdge(4,1);
+        graph.AddEdge(4,3);
 
         for (int i = 0; i < graph.max_vertex; i++) {
             graph.AddVertex(i + 100);
--- src/s10/Task10.java
+++ src/s10/Task10.java
@@ -8,22 +8,26 @@ public class Task10 {
         for (int i = 0; i < graph.max_vertex; i++) {
             graph.AddVertex(i+100);
         }
-        graph.AddEdge(0, 1);
-        graph.AddEdge(0, 2);
-        graph.AddEdge(0, 3);
-
-        graph.AddEdge(1, 3);
-        graph.AddEdge(1, 4);
-
-        graph.AddEdge(2, 3);
-
-        graph.AddEdge(3, 3);
-        graph.AddEdge(3, 4);
+        graph.AddEdge(0,1);
+        graph.AddEdge(0,2);
+        graph.AddEdge(0,3);
+        graph.AddEdge(1,0);
+        graph.AddEdge(1,3);
+        graph.AddEdge(1,4);
+        graph.AddEdge(2,0);
+        graph.AddEdge(2,3);
+        graph.AddEdge(3,0);
+        graph.AddEdge(3,1);
+        graph.AddEdge(3,2);
+        graph.AddEdge(3,3);
+        graph.AddEdge(3,4);
+        graph.AddEdge(4,1);
+        graph.AddEdge(4,3);
 
 
         System.out.println(graph);
-        int n = 2;
-        int m = 2;
+        int n = 0;
+        int m = 0;
         System.out.println("From " + n + " to " + m);
         for(Vertex i : graph.DepthFirstSearch(n,m)){
             System.out.print(i.Value + " ");
